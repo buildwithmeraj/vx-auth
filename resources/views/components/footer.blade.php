a866c96fdc6a76dafc7d456ab25aab7ffebb2465
Merajul Islam <buildwithmeraj@gmail.com>
2026-04-04 17:47:24 +0600

    created the layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+    <aside>
+        <p>Copyright © {{date("Y")}} - All right reserved by Vixlo Technologies</p>
+    </aside>
+</footer>
